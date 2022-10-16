7828059a1e42b9537da74671884be8d1b23135ca
lee <lee@DESKTOP-Q9DT31H>
2022-10-16 19:52:25 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// cards_len은 배열 cards의 길이입니다.
+int solution(int cards[], size_t cards_len) {
+    int answer = 0;
+    return answer;
+}
